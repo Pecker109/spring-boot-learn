c254b6c252e6c71701a5cecd81032ddb644d0420
pecker <pecker109@aliyun.com>
2021-04-08 15:38:02 +0800

    add:GitTest

---
+++ example/src/test/java/com/example/demo/GitTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+/**
+ * @author Pecker
+ * @Description 类信息
+ * @since 2021-04-08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+    }
+}
